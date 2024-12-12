fefcb14031e4bf5019a6e4386aa4f51cf7290718
vicmarchesi <victor.marchesi91@gmail.com>
2024-12-12 20:17:14 +0000

    Solved Lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0; 
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item); 
+    this.items.sort((a, b) => a - b); 
+    this.length = this.items.length; 
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length - 1];
+  }
 
   min() {}
 
